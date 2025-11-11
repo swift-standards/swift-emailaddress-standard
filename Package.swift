acc23837e9631ef07ae6a610dc235b68ad333652
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-11-11 11:57:20 +0100

    Add RFC 2822 AddrSpec conversion extensions
    Provides type-safe bidirectional conversions between EmailAddress and RFC_2822.AddrSpec using unlabeled initializers following Swift conventions.

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ extension Target.Dependency {
 }
 
 extension Target.Dependency {
+    static var rfc2822: Self { .product(name: "RFC_2822", package: "swift-rfc-2822") }
     static var rfc5321: Self { .product(name: "RFC_5321", package: "swift-rfc-5321") }
     static var rfc5322: Self { .product(name: "RFC_5322", package: "swift-rfc-5322") }
     static var rfc6531: Self { .product(name: "RFC_6531", package: "swift-rfc-6531") }
@@ -25,6 +26,7 @@ let package = Package(
         .library(name: .emailAddress, targets: [.emailAddress])
     ],
     dependencies: [
+        .package(url: "https://github.com/swift-web-standards/swift-rfc-2822", from: "0.0.1"),
         .package(url: "https://github.com/swift-web-standards/swift-rfc-5321", from: "0.0.1"),
         .package(url: "https://github.com/swift-web-standards/swift-rfc-5322", from: "0.0.1"),
         .package(url: "https://github.com/swift-web-standards/swift-rfc-6531", from: "0.0.1"),
@@ -35,6 +37,7 @@ let package = Package(
             name: .emailAddress,
             dependencies: [
                 .domain,
+                .rfc2822,
                 .rfc5321,
                 .rfc5322,
                 .rfc6531
---
+++ Sources/EmailAddress/EmailAddress+RFC2822.swift
@@ -0,0 +1,35 @@
+import Foundation
+import RFC_2822
+import RFC_5322
+
+// MARK: - EmailAddress ↔ RFC_2822.AddrSpec Conversions
+
+extension EmailAddress {
+    /// Initialize EmailAddress from RFC 2822 AddrSpec
+    public init(_ addrSpec: RFC_2822.AddrSpec) throws {
+        // RFC 2822 is obsoleted by RFC 5322, so we can treat the AddrSpec as RFC 5322 compatible
+        try self.init(
+            localPart: addrSpec.localPart,
+            domain: addrSpec.domain
+        )
+    }
+}
+
+extension RFC_2822.AddrSpec {
+    /// Initialize AddrSpec from EmailAddress
+    public init(_ emailAddress: EmailAddress) throws {
+        // Try to use RFC 5322 representation if available (most compatible)
+        if let rfc5322 = emailAddress.rfc5322 {
+            try self.init(
+                localPart: rfc5322.localPart.stringValue,
+                domain: rfc5322.domain.name
+            )
+        } else {
+            // Fall back to RFC 6531 (most permissive)
+            try self.init(
+                localPart: emailAddress.rfc6531.localPart.stringValue,
+                domain: emailAddress.rfc6531.domain.name
+            )
+        }
+    }
+}
