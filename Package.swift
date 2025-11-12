6cce9ca04370a56b1e55f983fc6df52f335a74bc
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-11-12 22:23:20 +0100

    chore: update RFC dependencies to 0.1.0 and fix domain-type URL

--- Package.swift
+++ Package.swift
@@ -26,11 +26,11 @@ let package = Package(
         .library(name: .emailAddress, targets: [.emailAddress])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-rfc-2822", from: "0.0.1"),
-        .package(url: "https://github.com/swift-standards/swift-rfc-5321", from: "0.0.1"),
-        .package(url: "https://github.com/swift-standards/swift-rfc-5322", from: "0.0.1"),
-        .package(url: "https://github.com/swift-standards/swift-rfc-6531", from: "0.0.1"),
-        .package(url: "https://github.com/coenttb/swift-domain-type", from: "0.0.1")
+        .package(url: "https://github.com/swift-standards/swift-rfc-2822", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-5321", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-5322", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-6531", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-domain-type", from: "0.1.0")
     ],
     targets: [
         .target(
