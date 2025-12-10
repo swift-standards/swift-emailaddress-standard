cbf4eb16a265eaeb1f77770acd048d106f913091
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 08:55:57 +0100

    Update dependency versions to latest tags

--- Package.swift
+++ Package.swift
@@ -30,11 +30,11 @@ let package = Package(
         .library(name: .emailAddress, targets: [.emailAddress])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-rfc-2822", from: "0.5.0"),
-        .package(url: "https://github.com/swift-standards/swift-rfc-5321", from: "0.3.0"),
-        .package(url: "https://github.com/swift-standards/swift-rfc-5322", from: "0.6.0"),
-        .package(url: "https://github.com/swift-standards/swift-rfc-6531", from: "0.3.0"),
-        .package(url: "https://github.com/swift-standards/swift-domain-standard", from: "0.3.0")
+        .package(url: "https://github.com/swift-standards/swift-rfc-2822", from: "0.5.3"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-5321", from: "0.3.3"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-5322", from: "0.7.1"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-6531", from: "0.3.3"),
+        .package(url: "https://github.com/swift-standards/swift-domain-standard", from: "0.4.2")
     ],
     targets: [
         .target(
