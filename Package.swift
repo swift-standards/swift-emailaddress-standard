94c4fe6e1714a343a6319ae1cbeecda98e3ca30f
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-03 15:03:21 +0200

    Converge dependency locations to their canonical organizations

--- Package.swift
+++ Package.swift
@@ -2,22 +2,6 @@
 
 import PackageDescription
 
-extension String {
-    static let emailAddress: Self = "EmailAddress Standard"
-}
-
-extension Target.Dependency {
-    static var emailAddress: Self { .target(name: .emailAddress) }
-}
-
-extension Target.Dependency {
-    static var rfc2822: Self { .product(name: "RFC 2822", package: "swift-rfc-2822") }
-    static var rfc5321: Self { .product(name: "RFC 5321", package: "swift-rfc-5321") }
-    static var rfc5322: Self { .product(name: "RFC 5322", package: "swift-rfc-5322") }
-    static var rfc6531: Self { .product(name: "RFC 6531", package: "swift-rfc-6531") }
-    static var domain: Self { .product(name: "Domain Standard", package: "swift-domain-standard") }
-}
-
 let package = Package(
     name: "swift-emailaddress-standard",
     platforms: [
@@ -43,27 +27,22 @@ let package = Package(
         .target(
             name: "EmailAddress Standard",
             dependencies: [
-                .domain,
-                .rfc2822,
-                .rfc5321,
-                .rfc5322,
-                .rfc6531,
+                .product(name: "Domain Standard", package: "swift-domain-standard"),
+                .product(name: "RFC 2822", package: "swift-rfc-2822"),
+                .product(name: "RFC 5321", package: "swift-rfc-5321"),
+                .product(name: "RFC 5322", package: "swift-rfc-5322"),
+                .product(name: "RFC 6531", package: "swift-rfc-6531"),
             ]
         ),
         .testTarget(
             name: "EmailAddress Standard Tests",
             dependencies: [
-                "EmailAddress Standard"
+                .target(name: "EmailAddress Standard")
             ]
         ),
     ],
     swiftLanguageModes: [.v6]
 )
-
-extension String {
-    var tests: Self { self + " Tests" }
-    var foundation: Self { self + " Foundation" }
-}
 
 for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
     let ecosystem: [SwiftSetting] = [
